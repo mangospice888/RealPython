f451c0422e4386b8784269ce887d7b4ea4b03921
Muoi Ly <muoi.ly@gmail.com>
2015-08-06 15:34:31 -0700

    add all files

---
+++ _config.py
@@ -0,0 +1,14 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+CSRF_ENABLED = True
+SECRET_KEY = 'my_precious'
+
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
